572400b8e225e770ca3a5115b46784aaaf745ff8
7-xin <1296771088@qq.com>
2021-01-29 10:22:23 +0800

    观察者模式

--- common/src/main/java/io/netty/util/concurrent/Future.java
+++ common/src/main/java/io/netty/util/concurrent/Future.java
@@ -23,72 +23,74 @@ import java.util.concurrent.TimeUnit;
  * The result of an asynchronous operation.
  */
 @SuppressWarnings("ClassNameSameAsAncestorName")
+/**
+ * todo Future 表示对未来的任务封装
+ *
+ * todo 接口继承了 JUC 中的 Future，并在其基础上增加了很多方法
+ */
 public interface Future<V> extends java.util.concurrent.Future<V> {
 
     /**
-     * Returns {@code true} if and only if the I/O operation was completed
-     * successfully.
+     * Returns {@code true} if and only if the I/O operation was completed successfully.
+     * todo 判断 io 是否成功返回
      */
     boolean isSuccess();
 
     /**
      * returns {@code true} if and only if the operation can be cancelled via {@link #cancel(boolean)}.
+     * todo 判断是否是 cancel() 方法取消
      */
     boolean isCancellable();
 
     /**
-     * Returns the cause of the failed I/O operation if the I/O operation has
-     * failed.
+     * Returns the cause of the failed I/O operation if the I/O operation has failed.
      *
      * @return the cause of the failure.
-     *         {@code null} if succeeded or this future is not
-     *         completed yet.
+     *         {@code null} if succeeded or this future is not completed yet.
+     * todo 返回 IO 失败的原因
      */
     Throwable cause();
 
     /**
-     * Adds the specified listener to this future.  The
-     * specified listener is notified when this future is
-     * {@linkplain #isDone() done}.  If this future is already
-     * completed, the specified listener is notified immediately.
+     * Adds the specified listener to this future.  The specified listener is notified when this future is {@linkplain #isDone() done}.
+     * If this future is already completed, the specified listener is notified immediately.
+     * todo 使用了观察者设计模式, 给这个 future 添加监听器，一旦 Future 完成，listener 立即被通知
      */
     Future<V> addListener(GenericFutureListener<? extends Future<? super V>> listener);
 
     /**
-     * Adds the specified listeners to this future.  The
-     * specified listeners are notified when this future is
-     * {@linkplain #isDone() done}.  If this future is already
-     * completed, the specified listeners are notified immediately.
+     * Adds the specified listeners to this future.
+     * The specified listeners are notified when this future is {@linkplain #isDone() done}.
+     * If this future is already completed, the specified listeners are notified immediately.
+     * todo 添加多个 listener
      */
     Future<V> addListeners(GenericFutureListener<? extends Future<? super V>>... listeners);
 
     /**
      * Removes the first occurrence of the specified listener from this future.
-     * The specified listener is no longer notified when this
-     * future is {@linkplain #isDone() done}.  If the specified
-     * listener is not associated with this future, this method
-     * does nothing and returns silently.
+     * The specified listener is no longer notified when this future is {@linkplain #isDone() done}.
+     * If the specified listener is not associated with this future, this method does nothing and returns silently.
      */
     Future<V> removeListener(GenericFutureListener<? extends Future<? super V>> listener);
 
     /**
      * Removes the first occurrence for each of the listeners from this future.
-     * The specified listeners are no longer notified when this
-     * future is {@linkplain #isDone() done}.  If the specified
-     * listeners are not associated with this future, this method
-     * does nothing and returns silently.
+     * The specified listeners are no longer notified when this future is {@linkplain #isDone() done}.
+     * If the specified listeners are not associated with this future, this method does nothing and returns silently.
+     * todo 移除多个 listener
      */
     Future<V> removeListeners(GenericFutureListener<? extends Future<? super V>>... listeners);
 
     /**
-     * Waits for this future until it is done, and rethrows the cause of the failure if this future
-     * failed.
+     * Waits for this future until it is done, and rethrows the cause of the failure if this future  failed.
+     * todo sync(同步) 等待着 future 的完成, 并且,一旦future失败了,就会抛出 future 失败的原因
+     * todo bind()是个异步操作,我们需要同步等待他执行成功
      */
     Future<V> sync() throws InterruptedException;
 
     /**
-     * Waits for this future until it is done, and rethrows the cause of the failure if this future
-     * failed.
+     * Waits for this future until it is done, and rethrows the cause of the failure if this future failed.
+     * todo 不会被中断的 sync等待
      */
     Future<V> syncUninterruptibly();
 
@@ -97,6 +99,7 @@ public interface Future<V> extends java.util.concurrent.Future<V> {
      *
      * @throws InterruptedException
      *         if the current thread was interrupted
+     * todo 等待
      */
     Future<V> await() throws InterruptedException;
 
@@ -156,6 +159,8 @@ public interface Future<V> extends java.util.concurrent.Future<V> {
      *
      * As it is possible that a {@code null} value is used to mark the future as successful you also need to check
      * if the future is really done with {@link #isDone()} and not rely on the returned {@code null} value.
+     * todo 无阻塞的返回Future对象, 如果没有,返回null
+     * todo 有时 future成功执行后返回值为null, 这是null就是成功的标识, 如 runnable 就没有返回值, 因此文档建议还要 通过isDone() 判断一下真的完成了吗
      */
     V getNow();
 
